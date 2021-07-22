5767d3242d7f01652006ba12157fa7b3633131ea
luigi <luigi@nexti.com>
2021-07-22 13:22:50 -0300

    Dependencias de testes

---
+++ test/com/exemplo/entidades/CaixaEletronicoTeste.java
@@ -0,0 +1,14 @@
+package com.exemplo.entidades;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CaixaEletronicoTeste {
+	
+	@Test
+	public void testeBasico() {
+		
+	}
+
+}
